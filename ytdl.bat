d71527d9f910c87f026b91c611ea30b53aa5d4c4
Blazzycrafter <39300111+Blazzycrafter@users.noreply.github.com>
2020-12-02 02:28:57 +0100

    added Curl check

--- ytdl.bat
+++ ytdl.bat
@@ -1,9 +1,28 @@
 @echo off
+color 0f
 Goto start
 
 
 :setup
+::check if curl installed if no install it
+if not exist "C:\Windows\System32\curl.exe" (
 
+color 0c
+echo.
+echo.
+echo: ATTENTION: Curl was not found please update your windows system or install curl manuelly:
+echo: https://curl.se/windows/dl-7.73.0_3/curl-7.73.0_3-win64-mingw.zip
+pause
+pause
+pause
+cls
+color 0f
+
+
+
+
+
+)
 echo Downloading YT-DL
 curl -L https://yt-dl.org/downloads/2020.11.26/youtube-dl.exe -o youtube-dl.exe -s
 
@@ -17,19 +36,15 @@ Goto c
 
 :start
 setlocal
-echo.
-echo.
-echo.
-echo.
-echo.
-echo.
-echo.
+
 if not exist youtube-dl.exe goto setup
 
 :c
-echo "Please set the youtube link"
+echo Please set the youtube link
 set /p vid="link>>>"
 
 
 
 call youtube-dl.exe "%vid%"
+echo Video downloaded
+pause
